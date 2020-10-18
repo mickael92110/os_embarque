7c08918280a1b5e32bd225d4ae22292dfeef854e
mickael92110 <mickael.ramilison@gmail.com>
2020-10-18 21:09:29 +0200

    Update rpi_gpio_test_simple.c

--- rpi_gpio_test_simple.c
+++ rpi_gpio_test_simple.c
@@ -1,47 +1,63 @@
-/*************************************************************************\
-  rpi_gpio_test_simple.c
-************************************************************************/
-
-//module simple qui met à 1 la pin GPIO 23
-
 #include <linux/module.h>
+#include <linux/timer.h>
 #include <linux/gpio.h>
+#include <linux/fs.h>
 
+// Sortie sur broche 18 (GPIO 24)
+#define RPI_GPIO_OUT 24
 
-// La sortie va sur la broche 16 (GPIO 23).
-#define RPI_IRQ_GPIO_OUT 23
+// Entree sur broche 16 (GPIO 23)
+#define RPI_GPIO_IN  23
 
+static struct timer_list rpi_gpio_1_timer;
 
-// Chargement du module .
-static int __init rpi_gpio_init(void)
+static void rpi_gpio_1_function (unsigned long unused)
 {
-
-
-    printk(KERN_INFO "------------- On initialise le module -------------\n");
-
-    int err;
-
-    // Demander l’accès au GPIO de sortie.
-    if ((err = gpio_request(RPI_IRQ_GPIO_OUT, THIS_MODULE->name)) != 0)
-        return err;
-
-    // Configurer la direction du GPIO de sortie.
-    if ((err = gpio_direction_output(RPI_IRQ_GPIO_OUT,1)) != 0) {
-        gpio_free(RPI_IRQ_GPIO_OUT);
-        return err;
-    }
-    gpio_set_value(RPI_IRQ_GPIO_OUT, 1);
-    printk(KERN_INFO "------------- On met la GPIO 23 a 1 -------------\n");
-
-    return 0;
+  static int value = 1;
+  value = 1 - value;
+  if (gpio_get_value(RPI_GPIO_IN) == 0)
+    value = 0;
+  gpio_set_value(RPI_GPIO_OUT, value);
+  mod_timer(& rpi_gpio_1_timer, jiffies+ (HZ >> 3));
 }
-// Déchargement du module.
-static void __exit rpi_gpio_exit(void)
+
+static int __init rpi_gpio_1_init (void)
 {
-    // Liberation des GPIO.
-    printk(KERN_INFO "------------- On libere les GPIO -------------\n");
-    gpio_free(RPI_IRQ_GPIO_OUT);
+  int err;
+
+  if ((err = gpio_request(RPI_GPIO_IN,THIS_MODULE->name)) != 0)
+    return err;
+  if ((err = gpio_request(RPI_GPIO_OUT,THIS_MODULE->name)) != 0) {
+    gpio_free(RPI_GPIO_IN);
+    return err;
+  }
+  if ((err = gpio_direction_input(RPI_GPIO_IN)) != 0) {
+    gpio_free(RPI_GPIO_OUT);
+    gpio_free(RPI_GPIO_IN);
+    return err;
+  }
+  if ((err = gpio_direction_output(RPI_GPIO_OUT,1)) != 0) {
+    gpio_free(RPI_GPIO_OUT);
+    gpio_free(RPI_GPIO_IN);
+    return err;
+  }
+
+  init_timer(& rpi_gpio_1_timer);
+  rpi_gpio_1_timer.function = rpi_gpio_1_function;
+  rpi_gpio_1_timer.data = 0; // non utilise
+  rpi_gpio_1_timer.expires = jiffies + (HZ >> 3);
+  add_timer(& rpi_gpio_1_timer);
+
+  return 0; 
 }
-module_init(rpi_gpio_init);
-module_exit(rpi_gpio_exit);
+
+static void __exit rpi_gpio_1_exit (void)
+{
+  del_timer(& rpi_gpio_1_timer);
+  gpio_free(RPI_GPIO_OUT);
+  gpio_free(RPI_GPIO_IN);
+}
+
+module_init(rpi_gpio_1_init);
+module_exit(rpi_gpio_1_exit);
 MODULE_LICENSE("GPL");
